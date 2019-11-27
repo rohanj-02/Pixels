f44e8c12d66c9ae3e7ef30fc446ad7572d64c498
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2019-11-27 19:00:13 +0530

    Update Wait.ino

--- Wait/Wait.ino
+++ Wait/Wait.ino
@@ -5,25 +5,41 @@
 #include "LedControl.h"
 
 
-LedControl lc = LedControl(12,10,11,12);
+LedControl lc = LedControl(13,11,12,8);
+LedControl ld = LedControl(10,8,9,4);
 
 
 void start() {
-    lc.clearDisplay(0);
+    ld.clearDisplay(0);
     for(int r = 0; r < 2; r++){
         for(int c = 0; c < 8; c++){
-            lc.setLed(0, r, c, HIGH);
+            ld.setLed(0, r, c, HIGH);
             delay(50);
         }
     }
     delay(1500);
-    lc.clearDisplay(0);
+    ld.clearDisplay(0);
 }
 
 int matrix[24][32];
 
+void clearscreen() {
+  for (int i = 0; i < 8; ++i) {
+      lc.clearDisplay(i);
+      ld.clearDisplay(i);
+  }
+}
+
+void clearmatrix() {
+  for (int i = 0; i < 24; ++i) {
+     for (int j = 0; j < 32; ++j)
+        matrix[i][j] = 0; 
+  }
+}
+
 
 void update(){
+
 
     for (int r = 0; r < 24; ++r) {
       for (int c = 0; c < 32; ++c){
@@ -31,11 +47,19 @@ void update(){
            if (r < 8) {
               lc.setLed(c/8, r, c%8, HIGH);
            }else if (r >= 8 && r < 16) {
-              lc.setLed(7 - c/8, r - 8, 7 - c%8, HIGH);
+              lc.setLed(7 - c/8, 7 - (r - 8), 7 - c%8, HIGH);
            }else {
-              lc.setLed(8 + c/8, r - 16, c % 8, HIGH);
+              ld.setLed(c/8, r % 16, c % 8, HIGH);
            }
-        } 
+        }else {
+          if (r < 8) {
+              lc.setLed(c/8, r, c%8, LOW);
+           }else if (r >= 8 && r < 16) {
+              lc.setLed(7 - c/8, 7 - (r - 8), 7 - c%8, LOW);
+           }else {
+              ld.setLed(c/8, r % 16, c % 8, LOW);
+           }
+        }
      }
   }
   
@@ -47,9 +71,17 @@ void setup() {
   for (int i = 0; i < 8; ++i) {
   lc.shutdown(i,false);
   // Set the brightness to a medium values
-  lc.setIntensity(i, 8);
+  lc.setIntensity(i, 4);
   // and clear the display
   lc.clearDisplay(i);
+  }
+
+  for (int i = 0; i < 8; ++i) {
+  ld.shutdown(i,false);
+  // Set the brightness to a medium values
+  ld.setIntensity(i, 4);
+  // and clear the display
+  ld.clearDisplay(i);
   }
 
 
@@ -61,25 +93,155 @@ void setup() {
   start();
 }
 
-void bar(int c, int start) {
+void dot(int x, int y) {
+  /*
+   * x, y are coordinates of point p
+   *  00
+   * 0p00
+   * 0000
+   *  00
+  */
+  
+  matrix[x - 1][y] = 1;
+  matrix[x - 1][y + 1] = 1;
+  matrix[x + 2][y] = 1;
+  matrix[x + 2][y + 1] = 1;
 
-    for (int i = start; i < 24 - start; ++i) {
-        matrix[i][c] = 1;
-        matrix[i][c + 1] = 1;
-    }
-    
+  
+  matrix[x][y] = 1;
+  matrix[x][y + 1] = 1;
+  matrix[x + 1][y + 1] = 1;
+  matrix[x + 1][y] = 1;
+
+  matrix[x][y - 1] = 1;
+  matrix[x + 1][y - 1] = 1;
+
+  matrix[x][y + 2] = 1;
+  matrix[x + 1][y + 2] = 1;
 }
 
-int height = 0;
+byte h0[] =
+{
+   B00000000,
+   B00000000,
+   B00000000,
+   B00011000,
+   B00011000,
+   B00000000,
+   B00000000,
+   B00000000
+};
+
+
+byte h1[] =
+{
+   B00000000,
+   B00000000,
+   B00000000,
+   B00000000,
+   B00011000,
+   B00011000,
+   B00000000,
+   B00000000
+};
+
+byte c[] =
+{
+   B00000000,
+   B00000000,
+   B00000000,
+   B01100110,
+   B01100110,
+   B00000000,
+   B00000000,
+   B00000000
+};
+
+byte c0[] =
+{
+   B00000000,
+   B00000000,
+   B00000000,
+   B01100000,
+   B01100110,
+   B00000110,
+   B00000000,
+   B00000000
+};
+
+byte c1[] =
+{
+   B00000000,
+   B00000000,
+   B00000000,
+   B00000110,
+   B01100110,
+   B01100000,
+   B00000000,
+   B00000000
+};
 
 void loop() {
    
-    update();
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(6,i,c[i]);
+  }
 
-    bar(4, 9);
-    bar(8, 7);
-    bar(16, 5);
-    bar(24, 7);
-    bar(28, 9);
-    delay(1000);
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(5,i,c[i]);
+  }
+
+  delay(100);
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(6,i,c0[i]);
+  }
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(5,i,c[i]);
+  }
+
+  delay(100);
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(6,i,c1[i]);
+  }
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(5,i,c[i]);
+  }
+
+  delay(100);
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(6,i,c[i]);
+  }
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(5,i,c0[i]);
+  }
+
+  delay(100);
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(6,i,c[i]);
+  }
+
+  for (int i = 0; i < 8; i++)
+  {
+    lc.setRow(5,i,c1[i]);
+  }
+
+  delay(100);
+
 }
+
